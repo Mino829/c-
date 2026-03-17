08660e24cb4d78e99c3bf45c6fbb7257ae8a549a
Mino829 <coutaiabu@gmail.com>
2026-03-17 22:01:07 +0900

    server

--- server.cpp
+++ server.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <sys/socket.h> 
 #include <netinet/in.h> 
-#include <unistd.h>     
+#include <unistd.h>    
+#include <cstring> 
 
 int main() {
     // 1. ソケット作成 
@@ -20,19 +21,41 @@ int main() {
     // 3. バインド (ポートの確保)
     if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
         std::cerr << "バインドに失敗しました\n";
-        close(server_fd);
         return 1;
     }
 
     // 4. 待機開始
     if (listen(server_fd, 3) < 0) {
         std::cerr << "待機状態への移行に失敗しました\n";
-        close(server_fd);
         return 1;
     }
     
     std::cout << "サーバーがポート8080で待機中..." << std::endl;
 
+    sockaddr_in client_address;
+    socklen_t client_addrlen = sizeof(client_address);
+
+    while (true){
+        //5. accept: クライアントからの接続を待つ
+        int client_socket = accept(server_fd, (struct sockaddr*)&client_address, &client_addrlen);
+        if(client_socket < 0){
+            std::cerr << "接続の受付に失敗しました\n";
+            continue;
+        }
+
+        //6. メッセージの受信
+        char buffer[1024] = {0}; //受信用のバッファを用意
+        recv(client_socket, buffer, sizeof(buffer), 0);
+        std::cout << "受信したメッセージ:\n" << buffer << std::endl;
+
+        std::cout << "\n--- クライアントが接続しました！ ---" << std::endl;
+        //7. メッセージの返信
+        const char* reply = "Hello from C++ Server!\n";
+        send(client_socket, reply, strlen(reply), 0);
+        std::cout << "返信を送信しました。" << std::endl;
+
+
+    }
     
     close(server_fd);
     
